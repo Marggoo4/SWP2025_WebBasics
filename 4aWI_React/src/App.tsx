5ac673321a2e3d4d73ec43258462cb2a30f35092
Marco <marco.bischof@student.htldornbirn.at>
2025-12-05 09:59:49 +0100

    3_Done

---
+++ 4aWI_React/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Card from "./components/Card";
+
+export default function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Card />
+      </div>
+  )
+}
---
+++ 4aWI_React/src/components/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Card({}: Props) {
+  return (
+    <div>I am a Card</div>
+  )
+}
